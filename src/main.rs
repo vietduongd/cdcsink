e1755214b85148d5868978ff21ae202c4704e6e3
workstation <vietduongd@gmail.com>
2026-04-10 11:47:23 +0700

    commit : fix table name has suffix _resync

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-﻿use std::{collections::HashMap, env, error::Error};
+use std::{collections::HashMap, env, error::Error};
 
 use chrono::Local;
 use dotenvy::dotenv;
@@ -67,10 +67,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("Received {} messages at {}", messages.len(), Local::now());
         let mut message_active: HashMap<String, Vec<&NatMessageReceive>> = HashMap::new();
         for msg in &messages {
-            let table_name = &msg.table_name;
-            if table_name.ends_with("_resync") {
-                continue;
+            let mut table_name = msg.table_name.clone();
+            if let Some(stripped) = table_name.strip_suffix("_resync") {
+                table_name = stripped.to_string();
             }
+            let table_name = &table_name;
             if !schema_cache.contains_key(table_name) {
                 // Table chưa tồn tại: nếu message_active đang có dữ liệu thì insert trước
                 if let Some(buffered) = message_active.remove(table_name) {
